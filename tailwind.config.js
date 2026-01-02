611baf226d03f6c57baeda3404c0911e58737fdb
AhmadFauziHacker <ahmadfauzy0510@gmail.com>
2026-01-03 00:10:26 +0700

    ganti foto

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,17 @@
 module.exports = {
-  darkMode: "class", // <--- penting
+  darkMode: "class",
   content: ["./index.html", "./src/**/*.{html,js}"],
   theme: {
     extend: {
-      colors: {},
+      keyframes: {
+        'infinite-scroll': {
+          '0%': { transform: 'translateX(0)' },
+          '100%': { transform: 'translateX(-50%)' },
+        },
+      },
+      animation: {
+        'infinite-scroll': 'infinite-scroll 20s linear infinite',
+      },
     },
   },
   plugins: [],
